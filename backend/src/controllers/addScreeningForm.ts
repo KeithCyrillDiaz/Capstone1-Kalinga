1c92f1ba707f7416f32938ee72b917bd82c0dcc9
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-04-12 10:55:05 +0800

    renamed server folder to backend

--- backend/src/controllers/addScreeningForm.ts
+++ backend/src/controllers/addScreeningForm.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createScreeningForm, getScreeningFormByApplicantID } from '../db/screeningForms';
+import { createScreeningForm, getScreeningFormByApplicantID } from '../models/ApplyAsDonor';
 import { random, passEncryption } from '../helpers/passwordEncryption'
 
 
