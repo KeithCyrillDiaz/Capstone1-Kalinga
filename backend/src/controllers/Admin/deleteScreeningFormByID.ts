31755468c45ea23d47be60a16f8010f316dabed3
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-04-20 14:20:41 +0800

    Modified Backend files, repolished returns and console.logs

--- backend/src/controllers/Admin/deleteScreeningFormByID.ts
+++ backend/src/controllers/Admin/deleteScreeningFormByID.ts
@@ -1,30 +1,53 @@
 import express from 'express'
-import { isDeleteScreeningForm } from '../../models/ApplyAsDonor'
+import { isDeleteScreeningForm, approvedScreeningForm } from '../../models/ApplyAsDonor'
 
 export const deleteScreeningForm = async (req: express.Request, res: express.Response) => {
     try{
 
         console.log(req.params.Applicant_ID)
+        const updateScreeningForm = await approvedScreeningForm(req.params.Applicant_ID, "Yes")
+        if(!updateScreeningForm){
+            return res.json({
+                messages: {
+                    code: 1,
+                    message: "None Exisiting Applicant"
+                }
+            }).status(400)
+        }
         const screeningForm = await isDeleteScreeningForm(req.params.Applicant_ID, "Deleted")
-        console.log("result: ",screeningForm)
         if(!screeningForm){
-            return res.status(400).json({
+            return res.json({
                 messages: {
                     code: 1,
                     message: "None Existing Applicant"
                 }
-            })
+            }).status(400)
         }
 
-        return res.status(200).json({
+        if(updateScreeningForm.isApproved !== "Yes" || screeningForm.isDeleted !== "Deleted"){
+            console.log("Failed to update Applicant Form")
+            return res.json({
+                messages: {
+                    code: 1,
+                    message: "Failed to update Applicant Form"
+                }
+            }).status(400)
+        }
+        console.log("Approved and Delete Applicant Successfully")
+        return res.json({
             messages: {
                 code: 0,
-                message: "Delete Applicant Successfully"
+                message: "Approved and Delete Applicant Successfully"
             },
             screeningForm
-        })
+        }).status(200)
 
     } catch (error) {
-
+        return res.json({
+            messages: {
+                code: 1,
+                message: "Internal Server Error"
+            }
+        }).status(500)
     }
 }
