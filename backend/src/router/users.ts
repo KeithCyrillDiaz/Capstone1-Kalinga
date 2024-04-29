e611e724112d815eeee31d45e9557cf426e4ea80
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-04-30 02:27:49 +0800

    commit for pull

--- backend/src/router/users.ts
+++ backend/src/router/users.ts
@@ -9,9 +9,6 @@ import {createRequest} from "../controllers/Requestor/RequestController";
 import {getAppointmentByUsertype} from '../controllers/Admin/Appointment/getAppointmentByUsertype';
 import { getRequestByID } from '../controllers/Admin/Appointment/getMakeRequest';
 import { getRequestByUserType } from '../controllers/Admin/Appointment/getRequestByUserType';
-import { logInUser, logOutUser } from '../controllers/LogInUser';
-import { isApproved } from '../controllers/isApproved';
-import { updateUserDetails } from '../controllers/updateUser';
 import { updateDonationStatus } from '../controllers/Admin/Appointment/updateAppointmentStatus';
 import { updateRequestStatus }  from '../controllers/Admin/Appointment/updateRequestStatus';
 import { getPendingRequests } from '../controllers/Requestor/getPendingRequest';
@@ -24,6 +21,9 @@ import { getRequestStats } from '../controllers/Requestor/getRequestStats';
 import { updateDonationComplete } from '../controllers/Admin/Appointment/updateDonationComplete'
 import { getCompletedDonation } from '../controllers/Donor/getCompletedDonation';
 import { getDonorStats } from '../controllers/Donor/getDonorStats'
+import { logInUser, logOutUser } from '../controllers/LogInUser';
+import { isApproved } from '../controllers/isApproved';
+import { updateUserDetails } from '../controllers/updateUser';
 
 export default (router: express.Router) => {
   
@@ -35,7 +35,6 @@ export default (router: express.Router) => {
     router.get('/kalinga/userLogout/:token', logOutUser)
     router.post('/kalinga/adminLoginIn', AdminLogIn)
     router.get('/kalinga/isApproved/:Applicant_ID', isApproved)
-
     
     //donor
     router.post('/kalinga/createAppointment', createAppointment);
@@ -46,8 +45,25 @@ export default (router: express.Router) => {
     router.get ('/kalinga/getRequestByID/:RequestID', getRequestByID)
     router.get('/kalinga/getRequestByUserType/:userType', getRequestByUserType)
 
+    router.post('/kalinga/updateUserInformation', updateUserDetails)
+
+    router.put('/kalinga/updateDonationStatus/:AppointmentDonorID', updateDonationStatus);
+    router.put('/kalinga/updateDonationComplete/:AppointmentDonorID', updateDonationComplete)
+    router.put('/kalinga/updateRequestStatus/:RequestID', updateRequestStatus)
+    router.put('/kalinga/updateCompleteStatus/:Requestor_ID', updateCompleteStatus)
 
 
+    router.get('/kalinga/getPendingRequests/:Requestor_ID', getPendingRequests);
+    router.get('/kalinga/getApprovedRequests/:Requestor_ID', getApprovedRequests);
+    router.get('/kalinga/getCompletedRequests/:Requestor_ID', getCompletedRequests);
+    router.get('/kalinga/getCompletedDonation/:Donor_ID', getCompletedDonation)
+
+    router.get ('/kalinga/getRequestStatus', getRequestStatus)
+
+    router.get('/kalinga/getOngoingDonation/:Donor_ID', getOngoingDonation)
+    router.get('/kalinga/getRequestStats/:Requestor_ID', getRequestStats)
+    router.get('/kalinga/getDonorStats/:Donor_ID', getDonorStats)
+    
 
     
 } 
