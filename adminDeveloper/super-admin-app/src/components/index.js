36a5478b00935b9347fe7bb5aa45cbccad834aec
Achelois04 <mabeverly.somodio@tup.edu.ph>
2024-05-05 23:52:11 +0800

    TRUE FILE

--- adminDeveloper/super-admin-app/src/components/index.js
+++ adminDeveloper/super-admin-app/src/components/index.js
@@ -2,5 +2,6 @@ import MobileChecker from "./mobileChecker";
 import Sidebar from "./sidebar";
 import PieChart from "./pieChart";
 import LoadPercentage from "./loadPercentage";
+import RequestpieChart from "./requestPieChart";
 
-export { MobileChecker, Sidebar, PieChart, LoadPercentage };
+export { MobileChecker, Sidebar, PieChart, LoadPercentage, RequestpieChart };
