a01ab7c319cf7959b6dbb4039dc01815abd3243b
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-05-07 13:28:29 +0800

    added new file for Kalinga Admin Web

---
+++ KalingaAdminWeb/adminWebApp/src/components/index.js
@@ -0,0 +1,6 @@
+import MobileChecker from "./mobileChecker";
+import Sidebar from "./sidebar";
+import PieChart from "./pieChart";
+import LoadPercentage from "./loadPercentage";
+
+export { MobileChecker, Sidebar, PieChart, LoadPercentage };
