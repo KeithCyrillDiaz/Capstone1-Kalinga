79111b6f125ff10e8ce7f83e5181320eb1c9652d
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-05-11 04:06:27 +0800

    remove auto navigate back

--- client/Kalinga/ProjectComponents/Donor/Profile/DonorSettngs/DonorNotification.js
+++ client/Kalinga/ProjectComponents/Donor/Profile/DonorSettngs/DonorNotification.js
@@ -28,7 +28,6 @@ export default function NotificationScreen() {
       [
         {
           text: "Okay",
-          onPress: () => navigate.goBack("DonorSettingScreen")
         }
       ]
     );
