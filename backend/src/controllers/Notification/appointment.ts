6a02303393eeff943ba3266db9ad9fa8c0d8340a
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-05-10 20:32:33 +0800

    modified to add notification in kalinga

---
+++ backend/src/controllers/Notification/appointment.ts
@@ -0,0 +1,88 @@
+import express from "express"
+import { getAppointmentByDonorID } from "../../models/Donor/DonorSetAppointmentModel"
+import { createNotification } from "../../models/Notification/Notification"
+
+
+export const sendUpdateStatusAppointmentNotification = async (
+    req: express.Request, res: express.Response, next: express.NextFunction) => {
+
+    try {
+
+        const { AppointmentDonorID } = req.params
+        const { DonationStatus } = req.body
+
+        const allowedStatuses = ["Decline", "Complete", "Ongoing"];
+
+            if (!AppointmentDonorID || !DonationStatus || !allowedStatuses.includes(DonationStatus))
+                {
+                    console.log("Invalid Input, bad Request")
+                    return res.json({
+                        messages: {
+                            code: 1,
+                            messages: "Invalid Input, bad Request"
+                        }
+                    }).status(400)
+                }
+
+            const Appointment = await getAppointmentByDonorID(AppointmentDonorID)
+
+            if(!Appointment){
+                console.log("Non Existing Appointment")
+                return res.json({
+                    messages: {
+                        code: 1,
+                        message: "Non Existing Appointment"
+                    }
+                }).status(404)
+            }
+
+            let newNotification
+            if (DonationStatus === "Ongoing") {
+                newNotification = {
+                    ownerID: Appointment.Donor_ID,
+                    title: `Appointment Update: Ongoing`,
+                    content: `Your appointment is now ongoing. Thank you for your contribution!`,
+                    milkBank: Appointment.location
+            };
+            } else if (DonationStatus === "Decline") {
+                newNotification = {
+                    ownerID: Appointment.Donor_ID,
+                    title: `Appointment Update: Declined`,
+                    content: `Unfortunately, your appointment has been declined. If you have any questions, please contact us.`,
+                    milkBank: Appointment.location
+            };
+            } else if (DonationStatus === "Complete") {
+                newNotification = {
+                    ownerID: Appointment.Donor_ID,
+                    title: `Appointment Update: Completed`,
+                    content: `Congratulations! Your appointment has been successfully completed. Thank you for your donation!`,
+                    milkBank: Appointment.location
+            };
+            }
+            
+            const result = await createNotification(newNotification)
+
+            if(!result) {
+                console.log("Failed to create Notification")
+                return res.json({
+                    messages: {
+                        code: 1,
+                        message: "Failed to create Notification"
+                    }
+                }).status(201)
+            }
+            console.log("Notification: ", result)
+            console.log("Successfully Send Notification")
+            next()
+
+    } catch (error) {
+        console.log("Internal Server Error", error)
+        return res.json({
+            message: {
+                code: 1,
+                message: "Internal Server Error"
+            },
+            error
+        }).status(500)
+    }   
+}
