15fc3e0d81d15bdac240542b75a67d2613503802
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-05-14 04:46:19 +0800

    modified files

--- backend/src/models/ApplyAsDonor.ts
+++ backend/src/models/ApplyAsDonor.ts
@@ -80,6 +80,7 @@ const imageSchema = new mongoose.Schema({
     userType: { type: String }, // Add userType field if needed
     owner: { type: String }, // Add owner field if needed
     ownerID: { type: String },
+    purpose: {type: String},
     createdAt: {type: Date, default: Date.now},
     updatedAt: {type: Date, default: Date.now},
 });
@@ -100,6 +101,7 @@ const fileSchema = new mongoose.Schema({
     userType: { type: String }, // Add userType field if needed
     owner: { type: String }, // Add owner field if needed
     ownerID: { type: String },
+    purpose: { type: String },
     createdAt: {type: Date, default: Date.now},
     updatedAt: {type: Date, default: Date.now},
 });
