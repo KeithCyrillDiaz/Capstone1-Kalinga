99f5abfa6e6f42052a2004ec2428930a911f029e
RogineCubelo <rogine.cubelo@gmail.com>
2024-05-20 23:40:19 +0800

    admin changes

--- adminDeveloper/super-admin-app/src/pages/admin/Appointments/Requestor/requestorAppointmentConfirmation.jsx
+++ adminDeveloper/super-admin-app/src/pages/admin/Appointments/Requestor/requestorAppointmentConfirmation.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import RequestConfirmModal from "../../../../modal/RequestConfirmModal";
+import RequestConfirmModal from "../../../../Modal/RequestConfirmModal";
 import RequestDeclineModal from "../../../../Modal/RequestDeclineModal";
 import AppointmentRequestDeclineModal from "../../../../Modal/AppointmentRequestDeclineModal";
 import axios from "axios";
