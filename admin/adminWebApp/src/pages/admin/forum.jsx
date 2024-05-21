8b7048d2c08d73ea06e8f677dcc26470dd09ac22
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-05-21 18:50:51 +0800

    modified forum

--- admin/adminWebApp/src/pages/admin/forum.jsx
+++ admin/adminWebApp/src/pages/admin/forum.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useNavigate } from "react-router-dom";
+import { fetchAllPost } from "../../api/Forum/forum";
+import { RenderPosts } from "../../components/forum/RenderPosts";
+
 
 export default function () {
   const navigate = useNavigate();
+
+  const [posts, setPosts] = useState([])
 
   const handleForum = () => {
     navigate("/admin/forum");
@@ -15,6 +20,16 @@ export default function () {
   const handleRejected = () => {
     navigate("/admin/rejected");
   };
+
+  const fetchPosts = async () => {
+    const response  = await fetchAllPost()
+    if(response)setPosts(response)
+      else console.log("Error Fetching Posts")
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  },[])
 
   return (
     <>
@@ -34,17 +49,10 @@ export default function () {
           <h1 className="text-5xl text-primary-default">Forum Moderation</h1>
         </div>
         <hr className="border-t-2 border-primary-default" />
-        <div className="grid grid-flow-row-dense xl:p-8 lg:py-8 lg:px-5 gap-y-6">
-          <div className="px-4 py-2 bg-white border border-primary-default rounded-2xl">
-            <div className="grid xl:grid-cols-[75%_auto] lg:grid-cols-[70%_auto] gap-x-6 py-4 items-center justify-center">
-              <div className="text-3xl text-justify text-primary-default ">
-              Sorry, this feature is not yet available right now. 
-              Rest assured, our team is hard at work developing new features to better serve our community. 
-              Your continued support means the world to us. Thank you for your patience!
-              </div>
-            </div>
+          <div className="grid grid-flow-row-dense xl:p-8 lg:py-8 lg:px-5 gap-y-6">
+            <RenderPosts data={posts}/>
+          
           </div>
-        </div>
       </section>
     </>
   );
