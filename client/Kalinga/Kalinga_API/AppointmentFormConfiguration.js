7d3f049702be1796d1eed5c853c8964c4186b548
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-06-06 04:47:04 +0800

    added files to enable dynamic coding in Appointment Forms

---
+++ client/Kalinga/Kalinga_API/AppointmentFormConfiguration.js
@@ -0,0 +1,19 @@
+import axios from "axios"
+import { BASED_URL } from "../MyConstants"
+import { getToken } from "../ProjectComponents/Authorization/checktoken"
+import { logOutUser } from "../ProjectComponents/Authorization/checktoken"
+
+
+export const getFormFormat = async({navigation}) => {
+    try {
+        const token = await getToken()
+        if (!token) await logOutUser({navigation: navigation})
+        const result = await axios.get(`${BASED_URL}/kalinga/getAppointmentsConfiguration/`,
+          {headers:{Authorization: `Bearer ${token}`}}
+        )
+        const {donationAppointmentConfig, requestAppointmentConfig} = result.data //result.data format
+        return result.data
+    } catch (error) {
+        console.log("Error: ",error)
+    } 
+  }
