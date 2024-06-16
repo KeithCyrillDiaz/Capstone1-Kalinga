5ee4d7bba98d67217ff835876200f70133bdcc7f
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-06-17 02:03:52 +0800

    test

--- client/Kalinga/MyConstants.js
+++ client/Kalinga/MyConstants.js
@@ -1,24 +1,2 @@
 
-//wifi at home for localHost
-// export const BASED_URL = "http://192.168.1.104:7000"
-
-//PC
-// export const BASED_URL = "http://192.168.1.127:7000"
-
-// export const BASED_URL = "http://192.168.161.93:7000"
-
-// export const BASED_URL = "http://192.168.254.106:7000"
-
-//vercel
-export const BASED_URL = "https://capstone1-kalinga.vercel.app/"
-
-//Data
-// export const BASED_URL = "http://192.168.39.93:7000"
-// https://capstone1-kalinga.onrender.com
-
-
-// export const BASED_URL = "http://192.168.1.5:7000"
-
-
-export const GOOGLE_MAPS_API_KEY = "AIzaSyCyAgVIPvYV-Q3hQQQ6XxGIfnXsm_aNrJ0";
-// export const GOOGLE_MAPS_API_KEY = "AIzaSyDr0y3U9C2bn3HKlPPhuRvEE6UrNzksKcQ";
+export const BASED_URL = "http://192.168.1.104:7000"
